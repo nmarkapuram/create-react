60d3be9fc42f71d6a05d348d2cbbf37500f2b4f2
Nagaraja Markapuram <nmarkapuram@gmail.com>
2026-03-25 10:27:51 +0530

    sample files

---
+++ app.js
@@ -0,0 +1,7 @@
+const heading = React.createElement('h1', {id: 'heading'}, 'Hello React from js');
+console.log(heading);
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(heading);
+
+    
+    console.log(root);
